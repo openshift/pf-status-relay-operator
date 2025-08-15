bdb0d5fd881e7c639e71d2824c372f4f1cc2db72
Marcelo <marguerr@redhat.com>
2025-08-15 15:50:33 +0200

    Add interface uniquess check in validating webhook
    The webhook checks during a create or update operation that
    the pflacpmonitor object does not have an interface that conflicts
    with an existing object. The operator is cancelled if this happens.
    
    Signed-off-by: Marcelo <marguerr@redhat.com>

--- api/v1alpha1/pflacpmonitor_webhook_test.go
+++ api/v1alpha1/pflacpmonitor_webhook_test.go
@@ -17,55 +17,166 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 var _ = Describe("PFLACPMonitor Webhook", func() {
+	var validator *pflacpmonitorValidator
+	var ctx context.Context
 
-	Context("When creating PFLACPMonitor under Validating Webhook", func() {
-		It("Should admit", func() {
-			pfMonitor := &PFLACPMonitor{
-				Spec: PFLACPMonitorSpec{
-					Interfaces: []string{
-						"eth0",
-						"eth1",
+	BeforeEach(func() {
+		ctx = context.Background()
+		// We need to register our custom resource type with the fake client's scheme
+		scheme := runtime.NewScheme()
+		err := AddToScheme(scheme)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Create a fake client for our tests
+		fakeClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+		validator = &pflacpmonitorValidator{Client: fakeClient}
+	})
+
+	Describe("ValidateCreate", func() {
+		Context("with invalid spec", func() {
+			It("should reject an empty interface", func() {
+				monitor := &PFLACPMonitor{
+					ObjectMeta: metav1.ObjectMeta{Name: "test-monitor", Namespace: "default"},
+					Spec: PFLACPMonitorSpec{
+						Interfaces: []string{"eth0", " "}, // Contains an empty string after trim
 					},
-				},
-			}
+				}
+				_, err := validator.ValidateCreate(ctx, monitor)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("interface cannot be empty"))
+			})
 
-			_, err := pfMonitor.ValidateCreate()
-			Expect(err).ToNot(HaveOccurred())
+			It("should reject duplicate interfaces within the same resource", func() {
+				monitor := &PFLACPMonitor{
+					ObjectMeta: metav1.ObjectMeta{Name: "test-monitor", Namespace: "default"},
+					Spec: PFLACPMonitorSpec{
+						Interfaces: []string{"eth0", "eth1", "eth0"},
+					},
+				}
+				_, err := validator.ValidateCreate(ctx, monitor)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("interfaces must be unique"))
+			})
 		})
 
-		It("Should deny if a required field is empty", func() {
-			pfMonitor := &PFLACPMonitor{
-				Spec: PFLACPMonitorSpec{
-					Interfaces: []string{
-						"eth0",
-						"   ",
+		Context("with conflicting existing resources", func() {
+			BeforeEach(func() {
+				// Pre-populate the fake client with an existing monitor
+				existingMonitor := &PFLACPMonitor{
+					ObjectMeta: metav1.ObjectMeta{Name: "existing-monitor", Namespace: "default"},
+					Spec: PFLACPMonitorSpec{
+						Interfaces: []string{"eth0", "eth1"},
 					},
-				},
-			}
+				}
+				err := validator.Client.Create(ctx, existingMonitor)
+				Expect(err).NotTo(HaveOccurred())
+			})
 
-			_, err := pfMonitor.ValidateCreate()
-			Expect(err).To(HaveOccurred())
+			It("should reject a new monitor that uses an already claimed interface", func() {
+				newMonitor := &PFLACPMonitor{
+					ObjectMeta: metav1.ObjectMeta{Name: "new-monitor", Namespace: "default"},
+					Spec: PFLACPMonitorSpec{
+						Interfaces: []string{"eth2", "eth0"}, // eth0 is already used
+					},
+				}
+				_, err := validator.ValidateCreate(ctx, newMonitor)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("interfaces [eth2 eth0] conflict with the ones from PFLACPMonitor existing-monitor"))
+			})
+
+			It("should allow a new monitor with unique interfaces", func() {
+				newMonitor := &PFLACPMonitor{
+					ObjectMeta: metav1.ObjectMeta{Name: "new-monitor", Namespace: "default"},
+					Spec: PFLACPMonitorSpec{
+						Interfaces: []string{"eth2", "eth3"},
+					},
+				}
+				_, err := validator.ValidateCreate(ctx, newMonitor)
+				Expect(err).NotTo(HaveOccurred())
+			})
 		})
 
-		It("Should deny if interfaces are not unique", func() {
-			pfMonitor := &PFLACPMonitor{
-				Spec: PFLACPMonitorSpec{
-					Interfaces: []string{
-						"eth0",
-						"eth1",
-						"eth0",
+		Context("with a valid spec and no conflicts", func() {
+			It("should successfully validate the resource", func() {
+				monitor := &PFLACPMonitor{
+					ObjectMeta: metav1.ObjectMeta{Name: "test-monitor", Namespace: "default"},
+					Spec: PFLACPMonitorSpec{
+						Interfaces: []string{"eth0", "eth1"},
 					},
-				},
-			}
-
-			_, err := pfMonitor.ValidateUpdate(nil)
-			Expect(err).To(HaveOccurred())
+				}
+				_, err := validator.ValidateCreate(ctx, monitor)
+				Expect(err).NotTo(HaveOccurred())
+			})
 		})
 	})
 
+	Describe("ValidateUpdate", func() {
+		var oldMonitor *PFLACPMonitor
+
+		BeforeEach(func() {
+			// Create a monitor that will be "updated"
+			oldMonitor = &PFLACPMonitor{
+				ObjectMeta: metav1.ObjectMeta{Name: "monitor-to-update", Namespace: "default"},
+				Spec: PFLACPMonitorSpec{
+					Interfaces: []string{"eth0"},
+				},
+			}
+			err := validator.Client.Create(ctx, oldMonitor)
+			Expect(err).NotTo(HaveOccurred())
+
+			// Create another monitor that causes a potential conflict
+			conflictingMonitor := &PFLACPMonitor{
+				ObjectMeta: metav1.ObjectMeta{Name: "conflicting-monitor", Namespace: "default"},
+				Spec: PFLACPMonitorSpec{
+					Interfaces: []string{"eth1"},
+				},
+			}
+			err = validator.Client.Create(ctx, conflictingMonitor)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should reject an update that introduces a conflict with another monitor", func() {
+			updatedMonitor := oldMonitor.DeepCopy()
+			updatedMonitor.Spec.Interfaces = []string{"eth0", "eth1"} // eth1 is used by conflicting-monitor
+
+			_, err := validator.ValidateUpdate(ctx, oldMonitor, updatedMonitor)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("interfaces [eth0 eth1] conflict with the ones from PFLACPMonitor conflicting-monitor"))
+		})
+
+		It("should allow an update that does not introduce any conflicts", func() {
+			updatedMonitor := oldMonitor.DeepCopy()
+			updatedMonitor.Spec.Interfaces = []string{"eth0", "eth2"} // eth2 is available
+
+			_, err := validator.ValidateUpdate(ctx, oldMonitor, updatedMonitor)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should allow an update that does not change the interfaces", func() {
+			updatedMonitor := oldMonitor.DeepCopy() // No changes to spec
+
+			_, err := validator.ValidateUpdate(ctx, oldMonitor, updatedMonitor)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should reject an update that introduces duplicate interfaces", func() {
+			updatedMonitor := oldMonitor.DeepCopy()
+			updatedMonitor.Spec.Interfaces = []string{"eth0", "eth0"}
+
+			_, err := validator.ValidateUpdate(ctx, oldMonitor, updatedMonitor)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("interfaces must be unique"))
+		})
+	})
 })
